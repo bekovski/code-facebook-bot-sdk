82a14a395c9a497a06603feeb715bed77e9f6424
Elias Bekovski <eliasbekovski@gmail.com>
2017-07-18 03:34:33 -0300

    first commit

---
+++ src/Message/Text.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace CodeBot\Message;
+
+Class Text
+{
+    private $recipientId;
+
+    public function __construct(string $recipientId)
+    {
+        $this->recipientId = $recipientId;
+    }
+
+
+    public function message(string $messageText): array
+    {
+        return [
+            'recipient' => [
+                'id' => $this->recipientId
+            ],
+            'message' => [
+                'text' => $messageText,
+                'metadata' => 'DEVELOPER_DEFINED_METADATA'
+
+            ]
+        ];
+    }
+
+}
