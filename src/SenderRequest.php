93e24d9294de81401ce9abf1fc9fd56db2fcf05e
Elias Bekovski <eliasbekovski@gmail.com>
2018-04-20 03:37:37 -0300

    Correção na maneira que recebe o evento.

--- src/SenderRequest.php
+++ src/SenderRequest.php
@@ -9,7 +9,7 @@ class SenderRequest
 
     public function __construct()
     {
-        $event = file_get_contents("php//input");
+        $event = file_get_contents("php://input");
         $event = json_decode($event, true, 512, JSON_BIGINT_AS_STRING);
         $this->event = $event['entry'][0]['messaging'][0];
     }
