c01a18a6850d6380bf97f2e76507cfad08e5053d
Elias Bekovski <eliasbekovski@gmail.com>
2018-04-20 08:18:32 -0300

    Alterado chave do array json recebido.

--- src/SenderRequest.php
+++ src/SenderRequest.php
@@ -9,9 +9,8 @@ class SenderRequest
 
     public function __construct()
     {
-        $event = file_get_contents("php://input");
-        $event = json_decode($event, true, 512, JSON_BIGINT_AS_STRING);
-        $this->event = $event['messaging'][0];
+        $event = json_decode(file_get_contents("php://input"), true, 512, JSON_BIGINT_AS_STRING);
+        $this->event = $event['entry'][0]['messaging'][0];
     }
 
     public function getSenderId()
